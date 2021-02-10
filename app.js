07b6acc8ff6a135c97fd83b404994c0022e237b5
Ibrahima Barry <ibrahimasfbarry@gmail.com>
2021-02-10 18:43:25 -0500

    Express Session Part 2

--- app.js
+++ app.js
@@ -42,33 +42,18 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
           const err = new Error('You are not authenticated!');
           res.setHeader('WWW-Authenticate', 'Basic');
           err.status = 401;
           return next(err);
-      }
-
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-          req.session.user = 'admin';
-          return next(); // authorized
-      } else {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
   } else {
-      if (req.session.user === 'admin') {
+      if (req.session.user === 'autheticated!') {
           return next();
       } else {
           const err = new Error('You are not authenticated!');
@@ -81,9 +66,23 @@ function auth(req, res, next) {
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
+function auth(req, res, next) {
+    console.log(req.session);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } else {
+        if (req.session.user === 'authenticated') {
+            return next();
+        } else {
+            const err = new Error('You are not authenticated!');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
