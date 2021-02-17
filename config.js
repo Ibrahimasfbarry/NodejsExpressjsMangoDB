291a0348b172153b363a14be2fa2439a4083cc7e
Ibrahima Barry <ibrahimasfbarry@gmail.com>
2021-02-16 20:26:27 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '267850374701199',
+        clientSecret: '19c6b9112d5f6732a3d9191d1b7faa1a'
+    }
 }
